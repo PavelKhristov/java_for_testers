ecb7819fb0989a8e67dc3c1b51d6d05a99d7e744
eJwtzMlqwzAUQNF/eWttXIXY0rKmUELrTUuGldDwlAorlniyMpJ/LwRvL5z7AG0tlvKbRpxAgrcNF52wxruVaTwXZt1xsRK608Z5a1zb2jfnG2CQKZ2DQ1roYzmpeVkd/5Ia6sd5O9y/R/65eR/6sHelO6wP+74tPxvL61e/q8DgRdR8ywgSDGpCAgbFplfQ7hQmmauJwaoRb4xQO5noyAhzYrUgSTzpENkl0ehjugADvOZAWJSeQU41xufzHyHQURo= <bfspawn1989@gmail.com>
2024-12-22 14:13:29 +0400

    Добавил запуск из конфигурационного файла

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -7,15 +7,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 
 public class ApplicationManager {
     protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private Properties properties;
 
 
-    public void init(String browser) {
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("firefox".equals(browser)){
                 driver = new FirefoxDriver();
@@ -25,9 +29,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Неизвестный браузер %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseURL"));
             driver.manage().window().setSize(new Dimension(1238, 993));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
