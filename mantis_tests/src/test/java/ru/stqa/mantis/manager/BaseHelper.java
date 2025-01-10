4f2a31ad7fadc85bd77eb7bcd19ab8c178595cff
eJwtzMlqwzAUQNF/eWttXIXY0rKmUELrTUuGldDwlAorlniyMpJ/LwRvL5z7AG0tlvKbRpxAgrcNF52wxruVaTwXZt1xsRK608Z5a1zb2jfnG2CQKZ2DQ1roYzmpeVkd/5Ia6sd5O9y/R/65eR/6sHelO6wP+74tPxvL61e/q8DgRdR8ywgSDGpCAgbFplfQ7hQmmauJwaoRb4xQO5noyAhzYrUgSTzpENkl0ehjugADvOZAWJSeQU41xufzHyHQURo= <bfspawn1989@gmail.com>
2025-01-10 15:03:04 +0400

    Добавил mantis_tests

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/BaseHelper.java
@@ -0,0 +1,36 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+
+import java.nio.file.Paths;
+
+public class BaseHelper {
+
+    public final ApplicationManager manager;
+
+    public BaseHelper(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        manager.driver().findElement(locator).click();
+    }
+
+    protected void type(By locator, String field) {
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(field);
+    }
+
+    void chooseElementFromSelector(By locator, String field) {
+        manager.driver().findElement(locator).sendKeys(field);
+    }
+
+    protected void attach(By locator, String file) {
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+    }
+
+    protected boolean isElementPresent (By locator){
+        return manager.driver().findElements(locator).size() > 0;
+    }
+
+}
