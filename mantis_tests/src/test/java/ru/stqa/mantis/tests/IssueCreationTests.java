14f570608c829a6034137f1d72ba0a72d3e7e657
eJwtzMlqwzAUQNF/eWttXIXY0rKmUELrTUuGldDwlAorlniyMpJ/LwRvL5z7AG0tlvKbRpxAgrcNF52wxruVaTwXZt1xsRK608Z5a1zb2jfnG2CQKZ2DQ1roYzmpeVkd/5Ia6sd5O9y/R/65eR/6sHelO6wP+74tPxvL61e/q8DgRdR8ywgSDGpCAgbFplfQ7hQmmauJwaoRb4xQO5noyAhzYrUgSTzpENkl0ehjugADvOZAWJSeQU41xufzHyHQURo= <bfspawn1989@gmail.com>
2025-01-19 16:20:10 +0400

    SOAP

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/IssueCreationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/IssueCreationTests.java
@@ -7,8 +7,16 @@ import ru.stqa.mantis.model.IssueData;
 public class IssueCreationTests extends TestBase{
 
     @Test
-    void canCreateIssue(){
+    void canCreateIssue1(){
         app.rest().createIssue(new IssueData()
+                .withSummary(CommonFunctions.randomString(10))
+                .withDescription(CommonFunctions.randomString(10))
+                .withProject(1L));
+    }
+
+    @Test
+    void canCreateIssue2(){
+        app.soap().createIssue(new IssueData()
                 .withSummary(CommonFunctions.randomString(10))
                 .withDescription(CommonFunctions.randomString(10))
                 .withProject(1L));
